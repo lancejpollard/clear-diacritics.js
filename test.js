916ded454c810f1995cdc04c5039848810ee48fe
Lance Pollard <lp@elk.fm>
2021-01-15 16:57:59 -0800

    Init

---
+++ test.js
@@ -0,0 +1,21 @@
+
+const clear = require('.')
+
+console.log(clear(`nālaṭiyār
+nāṉmaṇikkaṭikai
+iṉṉā nāṟpatu
+iṉiyavai nāṟpatu
+kār nāṟpatu
+kaḷavaḻi nāṟpatu
+aintiṇai aimpatu
+tiṉaimoḻi aimpatu
+aintinai eḻupatu
+tiṉaimalai nūṟṟu aimpatu
+tirukkuṛaḷ
+tirikaṭukam
+ācārakkōvai
+paḻamoḻi nāṉūṟu
+ciṟupañcamūlam
+mutumoḻikkānci
+elāti
+kainnilai`))
